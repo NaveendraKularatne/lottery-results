e6cd4ea47b353f6d99ebd164de951d948163640c
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-10-26 23:11:25 +0530

    Create entity

--- src/main/java/com/naveendra/lotteryresults/entity/GovisethaResults.java
+++ src/main/java/com/naveendra/lotteryresults/entity/GovisethaResults.java
@@ -4,13 +4,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Table(name = "lottery_results")
 public class GovisethaResults {
 
-    private Integer id
+    @Id
+    private Integer id;
+
+    @Column(name = "letter")
+    private String letter;
+
+    @Column(name = "number_01")
+    private Integer number01;
+
+    @Column(name = "number_02")
+    private Integer number02;
+
+    @Column(name = "number_03")
+    private Integer number03;
+
+    @Column(name = "number_04")
+    private Integer number04;
 }
