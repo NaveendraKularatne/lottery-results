43ac662707403df6d1742e1134eb762b98b6605f
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-10-27 01:48:38 +0530

    Create repository-GovisethaResultsRepository

---
+++ src/main/java/com/naveendra/lotteryresults/repository/GovisethaResultsRepository.java
@@ -0,0 +1,7 @@
+package com.naveendra.lotteryresults.repository;
+
+import com.naveendra.lotteryresults.entity.GovisethaResults;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GovisethaResultsRepository extends JpaRepository<GovisethaResults, Integer> {
+}
