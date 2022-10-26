f4d43a1ca5ecd577a752cc6e756ca2dccab191fb
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-10-26 22:25:58 +0530

    Initial commit

---
+++ src/main/java/com/naveendra/lotteryresults/entity/GovisethaResults.java
@@ -0,0 +1,16 @@
+package com.naveendra.lotteryresults.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class GovisethaResults {
+
+    private Integer id
+}
